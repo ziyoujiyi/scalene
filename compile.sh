b364f12a682abf1d726c59bf70471af27c622728
ziyoujiyi <997620387@qq.com>
2022-10-19 09:38:45 +0800

    .

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
+#vim /home/bwang/.local/lib/python3.7/site-packages/scalene.egg-link：/home/bwang/scalene
 rm -rf ./build/temp.linux-x86_64-cpython-37/scalene/libscalene.so
 rm -rf ./build/lib.linux-x86_64-cpython-37/scalene/libscalene.so
 rm -rf ./scalene/libscalene.so
